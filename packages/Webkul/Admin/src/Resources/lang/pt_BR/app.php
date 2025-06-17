6e9fd03185d4b482e228138a5b2f0a09dd0162e8
DivyaRevealer <150217503+DivyaRevealer@users.noreply.github.com>
2025-06-17 14:41:45 +0530

    Update app.php

--- packages/Webkul/Admin/src/Resources/lang/pt_BR/app.php
+++ packages/Webkul/Admin/src/Resources/lang/pt_BR/app.php
@@ -363,7 +363,7 @@ return [
 
         'layouts' => [
             'powered-by' => [
-                'description' => 'Desenvolvido por :krayin, um projeto de código aberto da :webkul.',
+                'description' => 'Desenvolvido por :revealer, um projeto de código aberto da :webkul.',
             ],
 
             'header' => [
