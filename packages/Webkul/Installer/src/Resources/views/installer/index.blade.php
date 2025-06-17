98ac3e8ef2a34291712faa9227ef6b183769b61a
DivyaRevealer <150217503+DivyaRevealer@users.noreply.github.com>
2025-06-17 16:21:39 +0530

    Update index.blade.php

--- packages/Webkul/Installer/src/Resources/views/installer/index.blade.php
+++ packages/Webkul/Installer/src/Resources/views/installer/index.blade.php
@@ -127,11 +127,11 @@
                 <!-- Left Side Welcome to Installation -->
                 <div class="flex flex-col justify-center">
                     <div class="m-auto grid h-[100vh] max-w-[362px] items-end">
-                        <div class="grid gap-4">
-                            <img
-                                src="{{ vite()->asset('images/logo.png', 'installer') }}"
-                                alt="@lang('installer::app.installer.index.krayin-logo')"
-                            >
+                           <img
+       src="{{ vite()->asset('images/logo.png', 'installer') }}"
+       alt="@lang('installer::app.installer.index.krayin-logo')"
+   >
+
 
                             <div class="grid gap-1.5">
                                 <p class="text-xl font-bold text-gray-800">
