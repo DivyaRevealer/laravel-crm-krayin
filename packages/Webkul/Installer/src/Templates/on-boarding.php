54fbd08964d5ffae017c9889fdded7db773bb874
DivyaRevealer <150217503+DivyaRevealer@users.noreply.github.com>
2025-06-17 15:36:49 +0530

    Update on-boarding.php

--- packages/Webkul/Installer/src/Templates/on-boarding.php
+++ packages/Webkul/Installer/src/Templates/on-boarding.php
@@ -10,7 +10,7 @@
 
 </>
 
-Welcome to the <info>Krayin</info> project! Krayin Community is an <comment>open-source CRM solution</comment>
+Welcome to the <info>Revealer</info> project! Revealer Community is an <comment>open-source CRM solution</comment>
 which is built on top of Laravel and Vue.js.
 
-Made with 💖  by the Krayin Team. Happy helping :)
+Made with 💖  by the Revealer Team. Happy helping :)
