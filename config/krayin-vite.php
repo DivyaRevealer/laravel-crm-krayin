6dcd22f29aee7ea77306b2641de9cda6055d1535
DivyaRevealer <150217503+DivyaRevealer@users.noreply.github.com>
2025-06-17 14:30:19 +0530

    Update krayin-vite.php

--- config/krayin-vite.php
+++ config/krayin-vite.php
@@ -3,7 +3,7 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | Krayin Vite Configuration
+    | Revealer Vite Configuration
     |--------------------------------------------------------------------------
     |
     | Please add your Vite registry here to seamlessly support the `assets` function.
