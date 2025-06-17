f8ee6549e3ae93e2cee4f64f5cd4c101d0ad4336
DivyaRevealer <150217503+DivyaRevealer@users.noreply.github.com>
2025-06-17 10:51:33 +0530

    Update index.blade.php

--- packages/Webkul/Admin/src/Resources/views/components/layouts/header/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/header/index.blade.php
@@ -14,15 +14,14 @@
             @else
                 <img
                     class="h-10 max-sm:hidden"
-                    src="{{ request()->cookie('dark_mode') ? vite()->asset('images/dark-logo.svg') : vite()->asset('images/logo.svg') }}"
+                     src="{{ vite()->asset('images/logo.png') }}"
                     id="logo-image"
                     alt="{{ config('app.name') }}"
                 />
 
                 <img
                     class="h-10 sm:hidden"
-                    src="{{ request()->cookie('dark_mode') ? vite()->asset('images/mobile-dark-logo.svg') : vite()->asset('images/mobile-light-logo.svg') }}"
-                    id="logo-image"
+                       src="{{ vite()->asset('images/logo.png') }}"
                     alt="{{ config('app.name') }}"
                 />
             @endif
@@ -142,9 +141,9 @@
                 return {
                     isDarkMode: {{ request()->cookie('dark_mode') ?? 0 }},
 
-                    logo: "{{ vite()->asset('images/logo.svg') }}",
+                    logo: "{{ vite()->asset('images/logo.png') }}",
 
-                    dark_logo: "{{ vite()->asset('images/dark-logo.svg') }}",
+                    dark_logo: "{{ vite()->asset('images/logo.png') }}",
                 };
             },
 
