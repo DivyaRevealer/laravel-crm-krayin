dd6572f40ecf1bc66c8c2cf06056a075ca198cfd
DivyaRevealer <150217503+DivyaRevealer@users.noreply.github.com>
2025-06-19 16:26:18 +0530

    Update forgot-password.blade.php

--- packages/Webkul/Admin/src/Resources/views/sessions/forgot-password.blade.php
+++ packages/Webkul/Admin/src/Resources/views/sessions/forgot-password.blade.php
@@ -76,11 +76,11 @@
         </div>
 
         <!-- Powered By -->
-        <div class="text-sm font-normal">
-            @lang('admin::app.components.layouts.powered-by.description', [
-                'krayin' => '<a class="text-brandColor hover:underline " href="https://revealercrm.com/">Revealer</a>',
-                'webkul' => '<a class="text-brandColor hover:underline " href="https://webkul.com/">Webkul</a>',
-            ]) 
+<div class="text-sm font-normal">
+    @lang('admin::app.components.layouts.powered-by.description', [
+        'krayin' => '<a class="text-brandColor hover:underline " href="https://revealercrm.com/">Revealer</a>',
+        'webkul' => '<a class="text-brandColor hover:underline " href="https://webkul.com/">Webkul</a>',
+    ]) 
         </div>
     </div>
 </x-admin::layouts.anonymous>
